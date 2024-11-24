c2db897b1b035259c75dbf897ba1f444a64990cd
Felipe Cavalca <flpsnocvla@gmail.com>
2024-11-24 01:17:37 -0300

    Adicionando suporte a sessões com Redis e criando a classe de gerenciamento de sessões

--- api/Attributes/Cache.php
+++ api/Attributes/Cache.php
@@ -6,6 +6,7 @@ use Attribute;
 use Bifrost\Core\Cache as CoreCache;
 use Bifrost\Core\Get;
 use Bifrost\Core\Post;
+use Bifrost\Core\Session;
 use Bifrost\Include\AtrributesDefaultMethods;
 use Bifrost\Interface\AttributesInterface;
 
@@ -17,16 +18,18 @@ class Cache implements AttributesInterface
     private string $key;
     private int $time;
     private CoreCache $cache;
+    private Session $session;
 
     public function __construct(...$p)
     {
         $post = new Post();
         $get = new Get();
+        $this->session = new Session();
         $this->key = serialize([
             "key" => $p[0],
             "POST" => $post,
             "GET" => $get,
-            "SESSION" => $this->getFieldsSession($p[3] ?? []),
+            "SESSION" => $this->getFieldsSession($p[2] ?? []),
         ]);
         $this->time = $p[1];
         $this->cache = new CoreCache();
@@ -56,7 +59,7 @@ class Cache implements AttributesInterface
     {
         $fields = [];
         foreach ($fieldsSession as $field) {
-            $fields[$field] = $_SESSION[$field];
+            $fields[$field] = $this->session->$field;
         }
         return serialize($fields);
     }
