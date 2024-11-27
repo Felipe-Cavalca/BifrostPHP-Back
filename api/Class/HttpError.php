3a019a27d6dca2d356f91e9c5ab6fbcd3be75830
Felipe Cavalca <flpsnocvla@gmail.com>
2024-11-26 22:45:49 -0300

    Adicionando informação adicional na representação de erro HTTP para instruções de uso do método OPTIONS

--- api/Class/HttpError.php
+++ api/Class/HttpError.php
@@ -27,11 +27,14 @@ class HttpError extends \Error
 
     public function __toString(): string
     {
+        $additionalInfo = array_merge($this->additionalInfo, [
+            "help" => "for more information send this request with OPTIONS method"
+        ]);
         return json_encode(HttpResponse::buildResponse(
             statusCode: $this->statusCode,
             message: $this->details,
             data: $this->data,
-            additionalInfo: $this->additionalInfo
+            additionalInfo: $additionalInfo
         ));
     }
 
