a384da99a56ff0fe6af3ff079c527ec56c658676
Felipe Cavalca <flpsnocvla@gmail.com>
2024-12-15 23:32:23 -0300

    Adicionando testes de exemplo utilizando PHPUnit

---
+++ api/tests/aa/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+    }
+
+    public function testExample2()
+    {
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+    }
+}
