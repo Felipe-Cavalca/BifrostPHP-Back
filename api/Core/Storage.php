2674810123e518f854a610a8a2cb3d31381a20ec
Felipe Cavalca <flpsnocvla@gmail.com>
2025-03-23 21:58:37 -0300

    Cria Classe de storage e desenvolve a função de upload de arquivos

---
+++ api/Core/Storage.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Bifrost\Core;
+
+use Bifrost\Core\Settings;
+use Bifrost\DataTypes\FilePath;
+use Bifrost\DataTypes\Base64;
+use Bifrost\DataTypes\ResponseStorage;
+use Bifrost\DataTypes\Url;
+
+class Storage
+{
+    private Settings $settings;
+    private Url $url;
+    private string $auth;
+
+    public function __construct()
+    {
+        $this->settings = new Settings();
+        $this->url = new Url($this->settings->STORAGE_HOST);
+        $this->auth = $this->settings->STORAGE_API_KEY;
+    }
+
+    public function set(FilePath $file, Base64 $base64): ResponseStorage
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this->url . $file);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+            "base64Content" => (string) $base64
+        ]));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer ' . $this->auth,
+            'Content-Type: application/json',
+        ]);
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            throw new \Exception('Curl error: ' . curl_error($ch));
+        }
+
+        curl_close($ch);
+
+        return new ResponseStorage(json_decode($response, true));
+    }
+
+    public function get(FilePath $file): Base64 {}
+
+    public function delete(FilePath $file) {}
+}
