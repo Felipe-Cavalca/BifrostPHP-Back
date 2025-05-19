d73880077ab169390d35eba2a1603c45674d46af
Felipe Cavalca <contato@felipecavalca.dev>
2025-05-18 23:26:59 -0300

    Adiciona a classe OptionalFields para validação de campos opcionais em endpoints

---
+++ api/Attributes/OptionalFields.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Bifrost\Attributes;
+
+use Attribute;
+use Bifrost\Class\HttpError;
+use Bifrost\Core\Post;
+use Bifrost\Enum\Field;
+use Bifrost\Include\AtrributesDefaultMethods;
+use Bifrost\Interface\AttributesInterface;
+
+#[Attribute]
+class OptionalFields implements AttributesInterface
+{
+
+    use AtrributesDefaultMethods;
+
+    public static array $fields = [];
+    private array $errors = [];
+
+    /**
+     * @param array $params - Campos opcionais a serem mostrados.
+     */
+    public function __construct(...$fields)
+    {
+        self::$fields = $fields[0];
+    }
+
+    /**
+     * Valida os campos opcionais e retorna erro caso algum campo não seja válido.
+     * @return mixed - Erro de requisição ou null caso não tenha erro.
+     */
+    public function beforeRun(): mixed
+    {
+        if (!$this->validateOptionalFields(self::$fields)) {
+            return HttpError::badRequest("Campos inválidos", $this->getErrors());
+        }
+        return null;
+    }
+
+    public function getOptions(): array
+    {
+        $campos = [];
+        foreach (self::$fields as $field => $filter) {
+            $campos[$field] = $filter->value ?? null;
+        }
+        return ["Optional Fields" => $campos];
+    }
+
+    /**
+     * Valida os campos opcionais e retorna erro caso algum campo não seja válido.
+     * @param array $fields - Campos opcionais para o endpoint.
+     * @return bool - True caso todos os campos sejam válidos, false caso contrário.
+     */
+    public function validateOptionalFields(array $fields): bool
+    {
+        $this->errors = [];
+
+        foreach ($fields as $field => $filter) {
+            if (is_int($field)) {
+                $field = $filter;
+                $filter = Field::DEFAULT;
+            }
+
+            if (!static::validateType($field, $filter) && empty($this->errors[$field])) {
+                $this->errors[$field] = "Tipo de campo inválido";
+            }
+        }
+
+        if (empty($this->errors)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Retorna os erros encontrados na validação dos campos opcionais.
+     * @return array - Erros encontrados na validação dos campos opcionais.
+     */
+    private function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    /**
+     * Valida o tipo do campo.
+     * @param string $field - Campo a ser validado.
+     * @param Field $filter - Tipo do campo a ser validado.
+     * @return bool - True caso o tipo do campo seja válido, false caso contrário.
+     */
+    private function validateType(string $field, Field $filter): bool
+    {
+        $post = new Post();
+        if (!isset($post->$field)) {
+            return true;
+        }
+        return $filter->validate($post->$field);
+    }
+}
