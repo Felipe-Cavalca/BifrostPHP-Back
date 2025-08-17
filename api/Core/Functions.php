75e2eb2712f89d1128f3563d8f6b8489e13d244c
Felipe Cavalca <contato@felipecavalca.dev>
2025-08-17 19:34:23 -0300

    Corrigindo funções sanitize

--- api/Core/Functions.php
+++ api/Core/Functions.php
@@ -5,15 +5,21 @@ namespace Bifrost\Core;
 class Functions
 {
 
-    public static function sanitize(string $value): string
+    public static function sanitize(mixed $value): string
     {
-        $value = str_replace("'", "''", $value);
-        $value = str_replace("\\", "\\\\", $value);
+        if (is_string($value)) {
+            $value = addslashes($value);
+        }
         return $value;
     }
 
     public static function sanitizeArray(array $data): array
     {
-        return array_map('addslashes', $data);
+        return array_map(function ($item) {
+            if (is_string($item)) {
+                return addslashes($item);
+            }
+            return $item; // mantém números e outros tipos intactos
+        }, $data);
     }
 }
