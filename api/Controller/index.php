c4a44fedf5f032755986382df60628a4a26f3324
Felipe Cavalca <contato@felipecavalca.dev>
2025-08-24 00:12:20 -0300

    Apagando dados do controller para deixar ele o mais simples possivel

--- api/Controller/index.php
+++ api/Controller/index.php
@@ -2,59 +2,15 @@
 
 namespace Bifrost\Controller;
 
-use Bifrost\Attributes\Cache;
-use Bifrost\Attributes\Details;
-use Bifrost\Attributes\Method;
-use Bifrost\Attributes\RequiredParams;
 use Bifrost\Class\HttpResponse;
-use Bifrost\Core\Get;
-use Bifrost\Core\Request;
-use Bifrost\Enum\Field;
-use Bifrost\Interface\ControllerInterface;
+use Bifrost\Interface\Controller;
+use Bifrost\Interface\Responseable;
 
-class Index implements ControllerInterface
+class Index implements Controller
 {
 
-    #[Cache(20)]
-    public function index()
+    public function index(): Responseable
     {
-        switch ($_SERVER["REQUEST_METHOD"]) {
-            case "OPTIONS":
-                return Request::run($this, "options_recurso");
-            case "GET":
-                return Request::run($this, "get_recurso");
-            default:
-                return HttpResponse::methodNotAllowed("Method not allowed");
-        }
-    }
-
-    #[Method("OPTIONS")]
-    #[Details(["description" => "Lista informações do recurso"])]
-    #[Cache(60)]
-    public function options_recurso()
-    {
-        return HttpResponse::success(
-            message: "Recurso",
-            data: [
-                "list_options" => Request::getOptionsAttributes($this, "options_recurso"),
-                "get_recurso" => Request::getOptionsAttributes($this, "get_recurso")
-            ]
-        );
-    }
-
-    #[Method("GET")]
-    #[Details([
-        "description" => "lista um recurso",
-    ])]
-    #[Cache(60)]
-    #[RequiredParams([
-        "id" => Field::UUID
-    ])]
-    public function get_recurso()
-    {
-        $get = new Get();
-        return HttpResponse::success("Recurso", [
-            "id" => $get->id,
-        ]);
+        return HttpResponse::success(message: "Operation completed successfully");
     }
 }
